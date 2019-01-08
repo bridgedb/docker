c29aa68e614dc52e751e84deabe8a65093b5dae6
Marvin Martens <marvin.martens@maastrichtuniversity.nl>
2019-01-08 14:17:49 +0100

    commit

--- doDocker.sh
+++ doDocker.sh
@@ -3,8 +3,8 @@ git clone https://github.com/swagger-api/swagger-ui.git
 
 docker build --no-cache -t "d" .
 #docker build -t p .
-sudo docker tag d bigcatum/bridgedb:develop-all-2
+sudo docker tag d bigcatum/bridgedb:orn-v4
 
 #sudo docker tag -f p dtlfair/search
-sudo docker push bigcatum/bridgedb:develop-all-2
+sudo docker push bigcatum/bridgedb:orn-v4
 
--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ cd /opt/
 mkdir bridgedb/
 cd bridgedb/
 
-wget https://bridgedb.org/data/releases/bridgedb-2.3.0.tar.gz
+wget https://www.bridgedb.org/data/releases/bridgedb-2.3.0.tar.gz
 tar -xvzf bridgedb-2.3.0.tar.gz bridgedb-2.3.0
 
 mkdir /opt/bridgedb-databases/
@@ -19,12 +19,13 @@ cd /opt/bridgedb-databases/
 
 export ENSEMBLMAIN="91"
 
-wget -r -l1 --no-parent -A "91.bridge" https://bridgedb.org/data/gene_database/
-wget -r -l1 --no-parent -A "39.bridge" https://bridgedb.org/data/gene_database/
+#wget -r -l1 --no-parent -A "91.bridge" https://bridgedb.org/data/gene_database/
+#wget -r -l1 --no-parent -A "39.bridge" https://bridgedb.org/data/gene_database/
 
-#wget -r -l1 --no-parent -A "Hs_Derby_Ensembl_${ENSEMBLMAIN}.bridge" https://www.bridgedb.org/data/gene_database/
-#wget -r -l1 --no-parent -A "Mm_Derby_Ensembl_${ENSEMBLMAIN}.bridge" https://www.bridgedb.org/data/gene_database/
-#wget -r -l1 --no-parent -A "Rn_Derby_Ensembl_${ENSEMBLMAIN}.bridge" https://www.bridgedb.org/data/gene_database/
+wget -r -l1 --no-parent -A "Hs_Derby_Ensembl_${ENSEMBLMAIN}.bridge" https://www.bridgedb.org/data/gene_database/
+wget -r -l1 --no-parent -A "Mm_Derby_Ensembl_${ENSEMBLMAIN}.bridge" https://www.bridgedb.org/data/gene_database/
+wget -r -l1 --no-parent -A "Rn_Derby_Ensembl_${ENSEMBLMAIN}.bridge" https://www.bridgedb.org/data/gene_database/
+wget -r -l1 --no-parent -A "Dr_Derby_Ensembl_${ENSEMBLMAIN}.bridge" https://www.bridgedb.org/data/gene_database/
 
 
 #wget -r -l1 --no-parent https://bridgedb.org/data/gene_database/Ag_Derby_Ensembl_Metazoa_32.bridge.zip
@@ -34,7 +35,7 @@ wget -r -l1 --no-parent -A "39.bridge" https://bridgedb.org/data/gene_database/
 
 
 cd /opt/bridgedb-databases/www.bridgedb.org/data/gene_database
-wget -c -O metabolites-20180809.bridge "https://ndownloader.figshare.com/files/12750080"
+wget -c -O metabolites-20181224.bridge "https://ndownloader.figshare.com/files/13902533"
 
 #cd bridgedb-2.2.1
 
