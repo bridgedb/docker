5ccb83bc7b60425a591d267c8d5c052c8d0a02a7
Marvin Martens <marvin.martens@maastrichtuniversity.nl>
2019-03-15 13:42:18 +0100

    minor changes

--- doDocker.sh
+++ doDocker.sh
@@ -1,10 +1,9 @@
-#git clone https://github.com/bridgedb/swagger-webservice.git
-git clone --single-branch --branch 2.x https://github.com/swagger-api/swagger-ui.git
+git clone https://github.com/bridgedb/swagger-webservice.git
+git clone https://github.com/swagger-api/swagger-ui.git
 
 docker build --no-cache -t "d" .
 #docker build -t p .
-sudo docker tag d bigcatum/bridgedb:all-v1
+sudo docker tag d bigcatum/bridgedb:all-v3
 
 #sudo docker tag -f p dtlfair/search
-sudo docker push bigcatum/bridgedb:all-v1
-
+sudo docker push bigcatum/bridgedb:all-v3
