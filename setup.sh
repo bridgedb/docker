ab198af62164108be972fa2c7b730cacccc2c508
Marvin Martens <marvin.martens@maastrichtuniversity.nl>
2019-03-27 16:17:55 +0100

    updated files for new bridgedb version and metabolite mapping

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ cd /opt/
 mkdir bridgedb/
 cd bridgedb/
 
-wget https://www.bridgedb.org/data/releases/bridgedb-2.3.0.tar.gz
-tar -xvzf bridgedb-2.3.0.tar.gz bridgedb-2.3.0
+wget https://www.bridgedb.org/data/releases/bridgedb-2.3.1.tar.gz
+tar -xvzf bridgedb-2.3.0.tar.gz bridgedb-2.3.1
 
 mkdir /opt/bridgedb-databases/
 cd /opt/bridgedb-databases/
@@ -33,7 +33,7 @@ wget -r -l1 --no-parent -A "*39.bridge" https://www.bridgedb.org/data/gene_datab
 #unzip *.zip
 
 cd /opt/bridgedb-databases/www.bridgedb.org/data/gene_database
-wget -c -O metabolites-20181224.bridge "https://ndownloader.figshare.com/files/13902533"
+wget -c -O metabolites-20190207.bridge "https://ndownloader.figshare.com/files/14298554"
 
 #cd bridgedb-2.2.1
 
