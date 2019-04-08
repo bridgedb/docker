dab5fb2eef71ebeecbdf1482a52282a93e144486
Marvin Martens <marvin.martens@maastrichtuniversity.nl>
2019-04-08 17:28:37 +0200

    adjusted with docker-specific webservice files and v2.3.1

--- doDocker.sh
+++ doDocker.sh
@@ -1,9 +1,8 @@
-git clone https://github.com/bridgedb/swagger-webservice.git
 git clone https://github.com/swagger-api/swagger-ui.git
 
 docker build --no-cache -t "d" .
 #docker build -t p .
-sudo docker tag d bigcatum/bridgedb:2.3.0
+sudo docker tag d bigcatum/bridgedb:2.3.1
 
 #sudo docker tag -f p dtlfair/search
 sudo docker push bigcatum/bridgedb
