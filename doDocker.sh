24dbd5cde39798324f9736b176ec38e98aa41058
marvinm2 <marvin.martens@maastrichtuniversity.nl>
2019-09-02 14:31:53 +0200

    testing with annotations

--- doDocker.sh
+++ doDocker.sh
@@ -1,8 +1,9 @@
 git clone https://github.com/swagger-api/swagger-ui.git
+git checkout -b bridgedb ccf17e90e2
 
 docker build --no-cache -t "d" .
 #docker build -t p .
-sudo docker tag d bigcatum/bridgedb:2.3.1
+sudo docker tag d bigcatum/bridgedb:2.3.1-test
 
 #sudo docker tag -f p dtlfair/search
-sudo docker push bigcatum/bridgedb
+sudo docker push bigcatum/bridgedb:2.3.1-test
